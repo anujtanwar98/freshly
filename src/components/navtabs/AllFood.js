8e8b47c7dea6c2c6eca3a7b10187b349b36d198e
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2023-11-14 01:52:57 -0500

    Update AllFood.js

--- src/components/navtabs/AllFood.js
+++ src/components/navtabs/AllFood.js
@@ -1,17 +1,26 @@
 import * as React from 'react';
-import { View, Text, SafeAreaView} from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 import { StyleSheet } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons';
-// <Ionicons name="scan" size={24} color="black" />
+import { Ionicons } from '@expo/vector-icons';
 
 
 const HomeMain = () => {
+  const handlePress = () => {
+    console.log("Icon button pressed!");
+    // Add your logic here for what should happen when the button is pressed
+  };
   return (
-    <SafeAreaView style={styles.mainWrapper}>
+    // <SafeAreaView style={styles.mainWrapper}>
+    <View style={styles.mainWrapper}>
       <View style={styles.container}>
-        <Text>Hello food 🍱</Text>
+        <Text style={styles.text}>My Food 🍱</Text>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Ionicons style={styles.icon} name="add-circle" color={'#FFA197'} size={30} />
+          <Text style={styles.AddText}>Add Food</Text>
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
+    // </SafeAreaView>
   );
 }
 
@@ -20,10 +29,38 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
+    paddingTop: 80,
     flex: 1,
     backgroundColor: '#00A2D6',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  icon: {
+    color: '#000',
+    fontSize: 26,
     alignItems: 'center',
   },
+  AddText: {
+    color: '#000',
+    fontSize: 14,
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: '#BDFFBE',
+    padding: 10,
+    borderRadius: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }
 })
 
 export default HomeMain;
