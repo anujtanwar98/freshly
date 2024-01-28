01cd3071263e2330c3a6eea4444de469b3d02626
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2024-01-28 14:36:52 -0500

    explore page styles

--- src/components/navtabs/Explore.js
+++ src/components/navtabs/Explore.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const Explore = () => {
     return (
-    <View style={styles.mainWrapper}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Explore 🧭</Text>
-      </View>
-  </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.mainWrapper}>
+          <View style={styles.container}>
+            <Text style={styles.text}>Explore</Text>
+          </View>
+          <View style={styles.searchContainer}>
+            <Text style={styles.searchTextContainer}><FontAwesome name="search" size={16} color="#8B8B8B" />   Search a food item....</Text>
+          </View>
+          <View>
+            <Text>Recent Items</Text>
+          </View>
+          <View style={styles.allCardContain}>
+            <TouchableOpacity style={styles.searchItemButton}>
+              <Text style={styles.buttonText}><MaterialIcons name="history" size={20} color="#7CC106" />  Carrots</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.searchItemButton}>
+              <Text style={styles.buttonText}><MaterialIcons name="history" size={20} color="#7CC106" />  Bread</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.searchItemButton}>
+              <Text style={styles.buttonText}><MaterialIcons name="history" size={20} color="#7CC106" />  Eggs</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.searchItemButton}>
+              <Text style={styles.buttonText}><MaterialIcons name="history" size={20} color="#7CC106" />  Cheese</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.searchItemButton}>
+              <Text style={styles.buttonText}><MaterialIcons name="history" size={20} color="#7CC106" />  Watermelon</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
 );
 }
 
@@ -18,23 +48,62 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-    paddingTop: 80,
     flex: 1,
-    backgroundColor: '#545F71',
+    backgroundColor: '#F3F3F3',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
     paddingHorizontal: 20,
   },
   text: {
-    color: '#fff',
+    color: '#7CC106',
     fontSize: 24,
     fontWeight: 'bold',
   },
-  icon: {
-    color: '#000',
-    fontSize: 26,
+  searchContainer: {
+    flex: 1,
+    backgroundColor: '#D8D8D8',
+    display: 'flex',
     alignItems: 'center',
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    borderRadius: 50,
+    height: 50,
+  },
+  searchTextContainer: {
+    color: '#8B8B8B',
+    fontSize: 16,
+    fontWeight: '500',
+    maxWidth: 300,
+    padding: 10,
+    marginLeft: 10,
+  },
+  allCardContain: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+    paddingHorizontal: 8,
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    gap: 20,
+  },
+  searchItemButton: {
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    flexDirection: 'column',
+    // justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 10,
+    borderColor: '#7CC106',
+    borderWidth: 1,
+    // width: 100,
   },
 })
 
