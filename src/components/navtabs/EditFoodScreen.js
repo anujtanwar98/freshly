178a702c32af4894ed04615092c5bc517f7869bc
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2024-02-06 19:55:36 -0500

    delete functionality :hammer: :lipstick:

--- src/components/navtabs/EditFoodScreen.js
+++ src/components/navtabs/EditFoodScreen.js
@@ -1,6 +1,6 @@
 // EditFoodScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity, SafeAreaView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNPickerSelect from 'react-native-picker-select';
 import { useFonts, PlusJakartaSans_500Medium, PlusJakartaSans_400Regular, PlusJakartaSans_600SemiBold } from '@expo-google-fonts/plus-jakarta-sans';
@@ -69,6 +69,33 @@ const EditFoodScreen = ({ route, navigation }) => {
       console.error('Error saving the updated name:', error);
       Alert.alert("Error", "Failed to update the item name");
     }
+  };
+
+  const deleteItem = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem('categorizedItems');
+      if (storedData !== null) {
+        let allCategories = JSON.parse(storedData);
+
+        // Loop through each category to find the item and remove it
+        Object.keys(allCategories).forEach(catKey => {
+          allCategories[catKey] = allCategories[catKey].filter(item => item.id !== itemId);
+        });
+
+        // Save the updated categories back to AsyncStorage
+        await AsyncStorage.setItem('categorizedItems', JSON.stringify(allCategories));
+
+        navigation.navigate('DetailScreen', { updatedItemId: itemId, updatedItemName: name });
+
+        // Optionally, show a success message or navigate back
+        Alert.alert("Success", "Item name updated successfully");
+        navigation.goBack(); // Go back to the previous screen
+      }
+    } catch (error) {
+      console.error('Error deleting the item:', error);
+      Alert.alert("Error", "Failed to delete the item");
+    }
+  };
 
   let [fontsLoaded] = useFonts({
     PlusJakartaSans_500Medium,
@@ -80,64 +107,132 @@ const EditFoodScreen = ({ route, navigation }) => {
     return null;
   }
 
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Edit Name:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setName}
-        value={name}
-      />
-      <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Edit Category:</Text>
-      <RNPickerSelect
-          onValueChange={(value) => setCategory(value)}
-          items={categories.map(cat => ({ label: cat, value: cat }))}
-          placeholder={{ label: 'Select a category...', value: null }}
-          style={pickerSelectStyles}
-          value={category} 
-        />
-      <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Edit Min Freshness Duration:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setMinFreshness}
-        value={minFreshness}
-        keyboardType="numeric"
-      />
-      <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Edit Max Freshness Duration:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setMaxFreshness}
-        value={maxFreshness}
-        keyboardType="numeric"
-      />
-      <Button
-        title="Save"
-        onPress={saveItemDetails}
-      />
-    </View>
+    <SafeAreaView style={[styles.safeView,{ flex: 1 }]}>
+      <View style={styles.container}>
+        <View style={styles.editNameBox}>
+          <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Item Name:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setName}
+            value={name}
+          />
+        </View>
+        <View style={styles.editNameBox}>
+          <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Category:</Text>
+          <RNPickerSelect
+              onValueChange={(value) => setCategory(value)}
+              items={categories.map(cat => ({ label: cat, value: cat }))}
+              placeholder={{ label: 'Select a category...', value: null }}
+              style={pickerSelectStyles}
+              value={category} 
+            />
+        </View>
+        <View style={styles.editFreshBox}>
+          <Text style={[styles.label, { fontFamily: 'PlusJakartaSans_600SemiBold' }]}>Days Fresh For:</Text>
+            <View style={styles.freshBoxWrapper}>
+              <View style={styles.editMinBox}>
+                <TextInput
+                    style={styles.freshForInput}
+                    onChangeText={setMinFreshness}
+                    value={minFreshness}
+                    keyboardType="numeric"
+                  />
+              </View>
+              <View style={styles.editMaxBox}>
+                <TextInput
+                  style={styles.freshForInput}
+                  onChangeText={setMaxFreshness}
+                  value={maxFreshness}
+                  keyboardType="numeric"
+                />
+              </View>
+            </View>
+        </View>
+        {/* <Button
+          title="Save"
+          onPress={saveItemDetails}
+        /> */}
+        <TouchableOpacity style={styles.saveButton} onPress={saveItemDetails} >
+          <Text style={styles.saveButtonText}>Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={deleteItem}>
+          <Text style={styles.deleteButtonText}>Delete</Text>
+        </TouchableOpacity>
+        {/* <Button
+          title="Delete"
+          onPress={deleteItem}
+        /> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeView: {
+    // backgroundColor: '#FBFBFB',
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
   },
-  label: {
-    marginBottom: 8,
+  editNameBox: {
+    width: '100%',
+    marginBottom: 12,
   },
-  input: {
+  freshBoxWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  editFreshBox: {
+    width: '100%',
+    // maxWidth: 180,
+    marginBottom: 12,
+  },
+  editMinBox: {
+    width: '100%',
+    maxWidth: 180,
+  },
+  editMaxBox: {
+    width: '100%',
+    maxWidth: 180,
+  },
+  freshForInput: {
     height: 40,
     width: '100%',
-    borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 12,
     paddingHorizontal: 8,
     fontFamily: 'PlusJakartaSans_500Medium',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#e9e9e9',
+    fontSize: 18,
+    color: '#163C16',
+    textAlign: 'center',
+  },
+  label: {
+    marginBottom: 8,
+    color: '#163C16',
+    fontSize: 14,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    fontFamily: 'PlusJakartaSans_500Medium',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#e9e9e9',
+    fontSize: 18,
+    color: '#163C16',
   },
   picker: {
     height: 50, // Make sure this height is enough
@@ -145,18 +240,49 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
     marginBottom: 12,
   },
+  saveButton: {
+    padding: 15,
+    marginBottom: 12,
+    width: '100%',
+    borderRadius: 44,
+    borderWidth: 1,
+    borderColor: '#168715',
+    backgroundColor: '#168715',
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    maxWidth: 300,
+  },
+  saveButtonText: {
+    color: '#ffffff',
+    fontFamily: 'PlusJakartaSans_600SemiBold',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  deleteButtonText: {
+    color: '#FF0000',
+    padding: 10,
+    borderRadius: 5,
+    fontFamily: 'PlusJakartaSans_600SemiBold',
+    textAlign: 'center',
+    fontSize: 14,
+  },
 });
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
-      fontSize: 16,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
+      height: 40,
+      width: '100%',
       borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 4,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
+      marginBottom: 12,
+      paddingHorizontal: 8,
       fontFamily: 'PlusJakartaSans_500Medium',
+      backgroundColor: '#ffffff',
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: '#e9e9e9',
+      fontSize: 18,
+      color: '#163C16',
   },
   inputAndroid: {
       fontSize: 16,
@@ -167,6 +293,19 @@ const pickerSelectStyles = StyleSheet.create({
       borderRadius: 8,
       color: 'black',
       paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  RNPickerSelect: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    fontFamily: 'PlusJakartaSans_500Medium',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#e9e9e9',
   },
 });
 
