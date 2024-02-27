7001e14ae45b422a1788aaf4a070ad9d110afa0e
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2024-02-27 16:34:45 -0500

    eatsoon styling chnages

--- src/components/navtabs/EatSoonTest.js
+++ src/components/navtabs/EatSoonTest.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, FlatList, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { useFonts, PlusJakartaSans_500Medium, PlusJakartaSans_400Regular, PlusJakartaSans_600SemiBold, PlusJakartaSans_700Bold, PlusJakartaSans_800ExtraBold } from '@expo-google-fonts/plus-jakarta-sans';
 
-const numColumns = 3;
-
 const EatSoon = ({ route }) => {
-  const { eatSoonItems } = route.params;
+    const { eatSoonItems } = route.params;
 
-  const eatWithin2Days = eatSoonItems.filter(item => item.freshness_duration_max <= 2);
-  const eatWithin5Days = eatSoonItems.filter(item => item.freshness_duration_max > 2);
-
-  const renderItem = ({ item, index }) => (
-    <View style={styles.eat_soon_container}>
-      <View style={styles.eat_soon_circle}>
-        <Text style={styles.eat_soon_emoji}>{item.emoji}</Text>
-        <Text style={item.freshness_duration_max <= 2 ? styles.eat_soon_subText_Red : styles.eat_soon_subText_Orange}>
-          {item.freshness_duration_min}-{item.freshness_duration_max} days
-        </Text>
-      </View>
-      <Text numberOfLines={1} style={styles.eat_soon_mainText}>{item.item}</Text>
-    </View>
-  );
+    const eatWithin2Days = eatSoonItems.filter(item => item.freshness_duration_max <= 2);
+    const eatWithin5Days = eatSoonItems.filter(item => item.freshness_duration_max > 2);
 
     let [fontsLoaded] = useFonts({
       PlusJakartaSans_500Medium,
@@ -33,50 +19,55 @@ const EatSoon = ({ route }) => {
       return null;
     }
 
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.main_eat_soon_title}>Eat Soon</Text>
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView style={styles.container}>
+          <View style={styles.all_items}>
+            <Text style={styles.main_eat_soon_title}>Eat Soon</Text>
 
-        {eatWithin2Days.length > 0 && (
-          <>
+            {eatWithin2Days.length > 0 && (
+            <>
             <Text style={styles.eat2SectionTitle}>Eat within 2 days</Text>
-            <View>
-            <FlatList
-              data={eatWithin2Days}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-              numColumns={numColumns}
-            />
+            <View style={styles.eat_soon_container_upper}>
+              {eatWithin2Days.map((item, index) => (
+                <View key={index} style={styles.eat_soon_container}>
+                  <View style={styles.eat_soon_circle}>
+                    <Text style={styles.eat_soon_emoji}>{item.emoji}</Text>
+                    <Text style={styles.eat_soon_subText_Red}>{item.freshness_duration_min}-{item.freshness_duration_max} days</Text>
+                  </View>
+                  <Text numberOfLines={1} style={styles.eat_soon_mainText}>{item.item}</Text>
+                </View>
+              ))}
             </View>
-          </>
-        )}
-        {eatWithin5Days.length > 0 && (
-          <>
+            </>
+            )}
+            {eatWithin5Days.length > 0 && (
+            <>
             <Text style={styles.eat5SectionTitle}>Eat within 5 days</Text>
-            <View>
-            <FlatList
-              data={eatWithin5Days}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-              numColumns={numColumns}
-            />
+            <View style={styles.eat_soon_container_lower}>
+              {eatWithin5Days.map((item, index) => (
+                <View key={index} style={styles.eat_soon_container}>
+                  <View style={styles.eat_soon_circle}>
+                    <Text style={styles.eat_soon_emoji}>{item.emoji}</Text>
+                    <Text style={styles.eat_soon_subText_Orange}>{item.freshness_duration_min}-{item.freshness_duration_max} days</Text>
+                  </View>
+                  <Text numberOfLines={1} style={styles.eat_soon_mainText}>{item.item}</Text>
+                </View>
+              ))}
             </View>
-          </>
-        )}
-      </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+            </>
+            )}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
 
 export default EatSoon;
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#FBFBFB',
   },
   container: {
     backgroundColor: '#FBFBFB',
@@ -108,7 +99,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     // margin: 10,
     marginLeft: 20,
-    marginBottom: 10,
     fontFamily: 'PlusJakartaSans_600SemiBold',
     color: '#E41C1C',
   },
@@ -117,16 +107,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     // margin: 10,
     marginLeft: 20,
-    marginTop: 20,
-    marginBottom: 10,
     fontFamily: 'PlusJakartaSans_600SemiBold',
     color: '#F78908',
   },
   eat_soon_container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    maxWidth: '33%',
   },
   eat_soon_circle: {
     width: 109,
